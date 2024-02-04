dcc004e9cb297d266aa99badc51b63b111f2d2cc
Prashant Malani <p.malani@gmail.com>
2024-02-04 00:08:57 -0800

    main: Read file into a vector

--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -1,3 +1,21 @@
+use std::{env, process::exit};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() != 2 {
+        println!("Invalid number of params:\n Usage is \"cargo run <filepath>\"");
+        exit(1);
+    }
+
+    // Read file into a vector.
+    let program = match std::fs::read(&args[1]) {
+        Ok(program) => program,
+        Err(_) => {
+            println!("Couldn't read file");
+            exit(1);
+        },
+    };
+
+    println!("Read in program of size: {} bytes", program.len());    
 }
