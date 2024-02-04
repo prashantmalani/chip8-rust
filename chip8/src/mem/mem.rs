61ee1f6fdef8d3c14a5b88530f523f72879d153a
Prashant Malani <p.malani@gmail.com>
2024-02-04 01:10:16 -0800

    mem: Add unit test to check load function

--- chip8/src/mem/mem.rs
+++ chip8/src/mem/mem.rs
@@ -1,9 +1,9 @@
 
+const MEM_SIZE: usize = 4096;
+const PROGRAM_ADDRESS: usize = 0x200;
 pub struct Memory {
     pub(crate) mem: [u8; 4096],
 }
-
-const PROGRAM_ADDRESS: usize = 0x200;
 
 impl Memory {
     // Program's are stored at 0x200 onwards
@@ -20,6 +20,14 @@ impl Memory {
 
         return Ok(0);
     }
+
+    pub fn read(&self, addr: usize) -> Result<u8, String> {
+        if addr >= MEM_SIZE {
+            return Err(String::from("Invalid read address."));
+        }
+
+        return Ok(self.mem[addr]);
+    }
 }
 
 #[cfg(test)]
@@ -31,5 +39,15 @@ mod tests {
         let large_program = vec![0; 4000];
         let mut mem = Memory{mem: [0; 4096]}; 
         assert!(mem.load_program(&large_program).is_err());
+    }
+
+    #[test]
+    fn check_load() {
+        let prog: Vec<u8> = vec![0x8; 400];
+        let mut mem = Memory{mem: [0; 4096]};
+        assert!(mem.load_program(&prog).is_ok());
+
+        assert_eq!(mem.read(crate::mem::mem::PROGRAM_ADDRESS).unwrap(), 0x8);
+        assert_eq!(mem.read(crate::mem::mem::PROGRAM_ADDRESS + prog.len() - 1).unwrap(), 0x8);
     }
 }
